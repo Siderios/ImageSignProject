e0d7ac4b59b921e2b4494a858ce4669b8dcb0eb0
D.ildo <34244107+Darkildo@users.noreply.github.com>
2023-12-20 18:43:03 +0300

    Функционал подписи приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_sign_project/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),home: const Scaffold(),
+      ),home: const HomeScreen(),
     );
   }
 }
